702d165de31e12421e9ee7b18fe6c4c8dfa0fdce
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-03-11 23:16:17 +0530

    Update regressionTestCases.java

--- src/test/java/twc/Regression/TestCases/regressionTestCases.java
+++ src/test/java/twc/Regression/TestCases/regressionTestCases.java
@@ -121,8 +121,9 @@ public class regressionTestCases extends TwcAndroidBaseTest {
 			Thread.sleep(3000);
 			System.out.println("Clicking on While using the app");
 			logStep("Clicking on While using the app");*/
-			Thread.sleep(5000);
+			Thread.sleep(10000);
 			AppiumFunctions.clickdailymaps();
+			Thread.sleep(20000);
 			this.proxy.getXml();
 			CharlesFunctions.createXMLFileForCharlesSessionFile();
 			Functions.get_Interstitial_aaxcal_Hourly();
